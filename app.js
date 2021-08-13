c5bd347768e7d10a74d2b2bfc2136ed4abf9fbdd
SCVORAl <systemked@mail.ru>
2021-08-13 17:57:50 +0300

    first

---
+++ app.js
@@ -0,0 +1,17 @@
+const Fastify = require('fastify')
+const server = Fastify({ logger: true })
+
+const PORT = process.env.PORT || 8080
+
+server.register(require('fastify-http-proxy'), {
+  upstream: 'https://api.kaktuz5.online',
+  prefix: '/',
+  rewritePrefix: '/v3/'
+})
+
+server.register(require('fastify-cors'), {
+  origin: '*',
+  methods: ['GET', 'HEAD', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+})
+
+server.listen(PORT)
