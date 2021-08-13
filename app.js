e1bdb0e64436dce367eebee2a0d783874d13d890
SCVORAl <systemked@mail.ru>
2021-08-13 19:07:23 +0300

    3

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const Fastify = require('fastify')
-const server = Fastify({ logger: true })
+// const Fastify = require('fastify')
+// const server = Fastify({ logger: true })
 
-const PORT = process.env.PORT || 8080
+// const PORT = process.env.PORT || 8080
 
-server.register(require('fastify-http-proxy'), {
-  upstream: 'https://api.kaktuz5.online',
-  prefix: '/',
-  rewritePrefix: '/v3/'
-})
+// server.register(require('fastify-http-proxy'), {
+//   upstream: 'https://api.kaktuz5.online',
+//   prefix: '/',
+//   rewritePrefix: '/v3/'
+// })
 
-server.register(require('fastify-cors'), {
-  origin: '*',
-  methods: ['GET', 'HEAD', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-})
+// server.register(require('fastify-cors'), {
+//   origin: '*',
+//   methods: ['GET', 'HEAD', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+// })
 
-server.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+// server.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+
+console.log('hjjgghjkl')
